f0e33a73cb0adcbb813c804f9ae4c7ba3415aee1
PSVAISHWARYA527 <psvsaishwarya@gmail.com>
2019-09-24 14:04:01 +0530

    s

---
+++ m1/src/AbsoluteNumber/AbsoluteNumber.java
@@ -0,0 +1,25 @@
+/**
+ * Given n, find the absolute value of n.
+ * 
+ * Input : abs(-1)
+ * Output : 1
+ * 
+ * Input : abs(0)
+ * Output : 0
+ * 
+ * @author Siva Sankar
+ */
+Package AbsoluteNumber;
+import java.lang.Math;
+public class AbsoluteNumber {
+
+    /**
+     * This method returns the absolute number of the given n
+     * @param n, the integer
+     * @return absolute value of n
+     */
+     
+     public static AbsoluteNumber abs(int n){
+       return (Math.abs(n));
+    }
+}
